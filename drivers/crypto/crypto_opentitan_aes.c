f5b5c6898619d2abe386005a2c28ab4a7a1c02f5
rknastenka <banabilalt@gmail.com>
2026-05-04 11:29:07 +0300

    include headers

--- drivers/crypto/crypto_opentitan_aes.c
+++ drivers/crypto/crypto_opentitan_aes.c
@@ -6,8 +6,24 @@
 ------------------------------------------------------
 2. Header Includes
 
-#include 
+#include <stdint.h>                 // For fixed-width integer types (uint32_t)
+#include <errno.h>                  // For error codes (-EINVAL, -ENOTSUP) returned by Zephyr APIs.
+#include <string.h>                 // For memset() and memcpy() used in buffer and key manipulations.
 
+#include <zephyr/kernel.h>          // the k_mutex used in our data struct for thread safety.
+#include <zephyr/device.h>          // DEVICE_DT_INST_DEFINE macro to register the driver instance with the OS.
+#include <zephyr/devicetree.h>      // For parsing the .dts files (e.g., DT_INST_REG_ADDR) to get hardware memory addresses.
+
+#include <zephyr/sys/sys_io.h>      // For sys_read32() and sys_write32() to read/write to the memory-mapped registers.
+#include <zephyr/sys/byteorder.h>   // To handle endianness when loading keys/data into the AES block.
+
+#include <zephyr/crypto/crypto.h>   // (crypto_session, crypto_pkt).
+#include <zephyr/crypto/cipher.h>   // (CRYPTO_CIPHER_MODE_CBC, CRYPTO_CIPHER_ALGO_AES).
+
+#include <zephyr/logging/log.h>      // For the LOG_ERR(), LOG_INF(): Debugging
+LOG_MODULE_REGISTER(opentitan_aes, CONFIG_CRYPTO_LOG_LEVEL);
+
+#include "aes_regs.h"            // just in case
 
 ------------------------------------------------------
 3. Register Offsets and Bitmasks (macro)
